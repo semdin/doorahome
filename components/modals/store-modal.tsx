fe7d8cbb9e93820dd380170ce00efb14b6973e1f
semdin <mehmetsemdin.aktay@std.yeditepe.edu.tr>
2023-08-10 18:49:45 +0300

    prisma-planetscale-mysql-setup-admin

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 import * as z from "zod";
+import axios from 'axios';
 import { useForm } from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod"
+import { useState } from "react";
+import {toast} from "react-hot-toast";
+
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+
 
 const formSchema = z.object({
     name: z.string().min(1),
@@ -18,6 +23,8 @@ const formSchema = z.object({
 export const StoreModal = () => {
     const storeModal = useStoreModal();
 
+    const [loading, setLoading] = useState(false);
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -26,7 +33,15 @@ export const StoreModal = () => {
     });
 
     const onSubmit = async ( values: z.infer<typeof formSchema>) => {
-        console.log(values);
+        try {
+            setLoading(true);
+            const response = await axios.post('/api/stores', values);
+            toast.success("Store created.");
+        } catch (error) {
+            toast.error("Something went wrong");
+        } finally{
+            setLoading(false);
+        }
     }
 
     return(
@@ -49,15 +64,15 @@ export const StoreModal = () => {
                                     Name
                                 </FormLabel>
                                 <FormControl>
-                                    <Input placeholder="doorahome" {...field} />
+                                    <Input disabled={loading} placeholder="doorahome" {...field} />
                                 </FormControl>
                                 <FormMessage></FormMessage>
                             </FormItem>
                         )}
                     />
                     <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                        <Button variant={"outline"} onClick={storeModal.onClose}>Cancel</Button>
-                        <Button type="submit">Continue</Button>
+                        <Button disabled={loading} variant={"outline"} onClick={storeModal.onClose}>Cancel</Button>
+                        <Button disabled={loading} type="submit">Continue</Button>
                     </div>
                 </form>
             </Form>
