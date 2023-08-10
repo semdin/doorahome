d5d2904d33340834a94d1d5869fcd88d717287f2
semdin <mehmetsemdin.aktay@std.yeditepe.edu.tr>
2023-08-10 14:35:55 +0300

    modal-components-admin

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,25 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+'use client';
+
+
+import { useEffect } from "react";
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 const SetupPage = () => {
-    return (
-      <div className="p-4">
-       <UserButton afterSignOutUrl="/"/>
-      </div>
-    )
-  }
+  //const storeModal = useStoreModal(); -- when using useEffect, it doesn't work properly.
+  const onOpen = useStoreModal((state)=>state.onOpen);
+  const isOpen = useStoreModal((state)=>state.isOpen);
 
-  export default SetupPage;
+  useEffect(()=>{
+    if(!isOpen){
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
   
+  return (
+    <div className="p-4">
+      Root Page
+    </div>
+  );
+}
+
+export default SetupPage;
