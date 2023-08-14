cab014299047c1dba3bc7cd83f601b87d4fd15b6
semdin <mehmetsemdin.aktay@std.yeditepe.edu.tr>
2023-08-14 09:20:36 +0300

    navigation-bar-admin

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,31 @@
-const Navbar = () => {
+import { UserButton, auth } from "@clerk/nextjs";
+import { MainNav } from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
+
+const Navbar = async () => {
+    const {userId} = auth();
+
+    if(!userId){
+        redirect("/sign-in");
+    }
+
+    const stores = await prismadb.store.findMany({
+        where:{
+            userId,
+        },
+    });
+
     return (  
-        <div>
-            This is a component navbar
+        <div className="border-b">
+            <div className="flex h-16 items-center px-4">
+                <StoreSwitcher items={stores}/>
+                <MainNav className="mx-6"/>
+                <div className="ml-auto flex items-center space-x-4">
+                    <UserButton afterSignOutUrl="/"/>
+                </div>
+            </div>
         </div>
     );
 }
