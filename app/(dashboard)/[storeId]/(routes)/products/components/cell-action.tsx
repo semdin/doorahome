e959b5340bdf9f164743e981a085dc497b35e680
semdin <mehmetsemdin.aktay@std.yeditepe.edu.tr>
2023-08-24 22:10:13 +0300

    products-entity

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import axios from "axios";
+import { useState } from "react";
+
+import { 
+    Copy, 
+    Edit, 
+    MoreHorizontal, 
+    Trash
+} from "lucide-react";
+
+import { 
+    DropdownMenu, 
+    DropdownMenuContent, 
+    DropdownMenuItem, 
+    DropdownMenuLabel, 
+    DropdownMenuTrigger 
+} from "@/components/ui/dropdown-menu";
+
+import { ProductColumn } from "./columns";
+import { Button } from "@/components/ui/button";
+import toast from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+import { AlertModal } from "@/components/modals/alert-modal";
+
+interface CellActionProps {
+    data: ProductColumn;
+}
+
+
+export const CellAction: React.FC<CellActionProps> = ({
+    data
+}) => {
+    const router = useRouter();
+    const params = useParams();
+
+    const [loading, setLoading] = useState(false);
+    const [open, setOpen] = useState(false);
+
+    const onCopy = (id: string) =>{
+        navigator.clipboard.writeText(id);
+        toast.success("Product Id has been copied to the clipboard.")
+    };
+
+
+    const onDelete = async () => {
+        try {
+            setLoading(true)
+            await axios.delete(`/api/${params.storeId}/products/${data.id}`)
+            router.refresh();
+            //router.push("/"); // I may put it last because success alert comes after routing to root ("/")
+            toast.success("Product has been deleted.")
+        } catch (error) {
+            toast.error("Something went wrong.")
+        } finally{
+            setLoading(false);
+            setOpen(false);
+        }
+        
+    }
+
+    return (
+        <>
+            <AlertModal 
+                isOpen = {open}
+                onClose = {()=> setOpen(false)}
+                onConfirm={onDelete}
+                loading={loading}
+            />
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="h-7 w-8 p-0">
+                        <span className="sr-only">Open Menu</span>
+                        <MoreHorizontal className="h-4 w-4"/>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>
+                        Actions
+                    </DropdownMenuLabel>
+                    <DropdownMenuItem onClick={()=> onCopy(data.id)}>
+                        <Copy className="mr-2 h-4 w-4"/>
+                        Copy Id
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={()=> router.push(`/${params.storeId}/products/${data.id}`)}>
+                        <Edit className="mr-2 h-4 w-4"/>
+                        Update
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={()=> setOpen(true)}>
+                        <Trash className="mr-2 h-4 w-4"/>
+                        Delete
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
+    );
+};
